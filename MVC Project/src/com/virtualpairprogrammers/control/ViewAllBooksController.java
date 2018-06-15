3a2d99aba63e9d6c23f9c87edece2279d04bcfac
anderson <anderson@java-devops>
2018-06-15 16:19:50 -0400

    Implemented autowire and annotations

--- MVC Project/src/com/virtualpairprogrammers/control/ViewAllBooksController.java
+++ MVC Project/src/com/virtualpairprogrammers/control/ViewAllBooksController.java
@@ -2,19 +2,19 @@ package com.virtualpairprogrammers.control;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.virtualpairprogrammers.domain.Book;
 import com.virtualpairprogrammers.services.BookService;
 
+@Controller
 public class ViewAllBooksController
 {
+	@Autowired
 	private BookService bookService;
-
-	public void setBookService(BookService bookService) {
-		this.bookService = bookService;
-	}
 	
 	@RequestMapping("/viewAllBooks")
 	public ModelAndView viewAllBooks()
