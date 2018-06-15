197896eadf84157467b485155a2ad174e88ffc81
anderson <anderson@java-devops>
2018-06-15 16:53:20 -0400

    Fusing similar controllers together
    modified the controllers to run from just one class

--- MVC Project/src/com/virtualpairprogrammers/control/BookManagementController.java
+++ MVC Project/src/com/virtualpairprogrammers/control/BookManagementController.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.virtualpairprogrammers.domain.Book;
 import com.virtualpairprogrammers.services.BookService;
 
 @Controller
-public class ViewAllBooksController
+public class BookManagementController
 {
 	@Autowired
 	private BookService bookService;
@@ -21,5 +22,12 @@ public class ViewAllBooksController
 	{
 		List<Book> allBooks = bookService.getEntireCatalogue();
 		return new ModelAndView("/displayAllBooks.jsp", "allBooks", allBooks);
+	}
+	
+	@RequestMapping("/findByAuthor")
+	public ModelAndView findByAuthor(@RequestParam("AUTHOR") String author)
+	{
+		List<Book> books = bookService.getAllBooksByAuthor(author);
+		return new ModelAndView("/displayAllBooks.jsp", "allBooks", books);
 	}
 }
